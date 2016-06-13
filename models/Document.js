b221ecaf2ab772fe9075a3dbb57ed9a68d6bb394
Anton Zuev <anzuev@bk.ru>
2016-06-13 20:55:52 +0300

    0.2.3

--- models/Document.js
+++ models/Document.js
@@ -1,6 +1,9 @@
 'use strict';
 
-let Schema = require('mongoose').Schema;
+let Schema = require('mongoose').Schema,
+	Q = require('q'),
+	DbError = require('@anzuev/studcloud.errors').DbError,
+	Util = require('util');
 
 let Document = new Schema({
 	title:{
@@ -61,5 +64,24 @@ let Document = new Schema({
 },{
 	collection: 'documents'
 });
+
+
+Document.statics.getDocumentById = function(id){
+	let deffer = Q.defer();
+	var promise = this.findById(id).exec();
+
+	promise.then(function(file){
+		if(file) deffer.fulfill(file);
+		else {
+			throw new DbError(null, 404, Util.format('No file found by id %s', id));
+		}
+	}).catch(function(err){
+		console.log(err);
+		if(err) deffer.reject(new DbError(err, 500));
+	});
+
+	return deffer.promise;
+};
+
 
 module.exports = Document;
